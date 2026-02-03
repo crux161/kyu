462a1cd4b95590512d34241b2f00f6b9ed1e65dd
crux161 <eain161@gmail.com>
2026-02-02 22:52:43 -0800

    fixed test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 time ./qq -c ./big.txt ./big.qq
 time ./qq -d ./big.qq ./big_q.txt
-ls -lah && \
+ls -lah *.qq *.txt && \
 diff big.txt big_q.txt && \
 sha256sum ./big.txt && \
 sha256sum ./big_q.txt
